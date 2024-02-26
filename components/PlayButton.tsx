60b765e02344df902371dd1bb683daa9bf6b46c5
Siqah <faruoqmuhammed@zetech.ac.ke>
2024-02-26 23:54:49 +0300

    playbutton done

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -1,10 +1,27 @@
+import { FaPlay } from "react-icons/fa";
 
 
 const PlayButton = () => {
     return (
-        <div>
-            PlayButton
-        </div>
+        <button  
+         className ="
+         transition 
+         opacity-0
+         rounded-full
+         flex
+         items-center
+         bg-green-500
+         p-4
+         drop-shadow-md
+         translate
+         translate-y-1/4   *:
+         group-hover:opacity-100
+         group-hover:translate-y-0
+         hover:scale-110
+         "opacity-100
+        >
+            <FaPlay  className= "text-black"/>
+        </button>
     )
 }
 
